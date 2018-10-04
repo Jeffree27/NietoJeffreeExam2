146a36da23e62356da1bf42d63fae94eeb8757ff
cisco 2018 <cisco2018@IICSGENERAL.com>
2018-10-04 15:26:44 +0800

    Initial commit

---
+++ app/src/main/java/com/nietojeffreeexam1/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.nietojeffreeexam1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i = new Intent(this, myCustomService.class);
+        startService(i);
+    }
+
+    public void processB(View v) {
+        Intent i =null, chooser =null;
+        if(v.getId() == R.id.button3){
+            i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        }else if(v.getId() == R.id.button3) {
+            i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        }
+    }
+}
---
+++ app/src/main/java/com/nietojeffreeexam1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.nietojeffreeexam1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, myCustomService.class);
+        startService(i);
+    }
+
+    public void processA(View v) {
+        Intent i =null, chooser =null;
+        if(v.getId() == R.id.button2){
+            i = new Intent(this,Main2Activity.class);
+            startActivity(i);
+        }else if(v.getId() == R.id.button) {
+            i =new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo://14.610420,120.991937"));
+            chooser = Intent.createChooser(i,"Choose a Map App");
+            startActivity(chooser);
+        }
+    }
+}
